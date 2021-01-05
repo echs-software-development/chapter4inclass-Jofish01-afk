a7e08bebc2af02e67db58f9f134d0e24c0e50614
JOSEPH ESCOBAR <je0887@students.necsd.net>
2021-01-05 15:31:36 +0000

    Each problem. (1, 2, 3, 4)

--- Prob2.java
+++ Prob2.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Prob2{
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("This program will find the difference between two numbers.");
+    
+    System.out.println("Enter two numbers: ");
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
 
+    //math.abs
+    int num3 = Math.abs(num1 - num2);
+    System.out.println(num3);
 
   }
 }
